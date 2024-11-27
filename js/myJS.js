76c324da0bf743ca3ebdd6e0923bbf856d0c0298
khalilTaj <khalil.taj.aldean@gmail.com>
2024-11-27 03:40:11 +0200

    showcaing diffrent varuable forms and playing around with numbers

--- js/myJS.js
+++ js/myJS.js
@@ -19,6 +19,24 @@ console.log(age);
 const pi = 3.14159;
 console.log(pi);
 
-pi = 50;
-console.log(pi);
+// pi = 50;
+// console.log(pi);
 
+console.log("this is the start of the assigment");
+console.log(23+97);
+console.log(258974+458621);
+let a = 10;
+console.log(a);
+console.log(9*a);
+let b = 7*a;
+console.log(b);
+
+let max = 57;
+let actual = max - 13;
+let percent = actual/max;
+let $ = 1;
+let _ = 1;
+// let 1A = 5; --> shows an error, can't start with a digit
+// let return = 4; --> shows an error 'return' cant be a var
+// let let = 5; --> shows an error can't have 'let' as a var
+num = 5; //works jsut fine
