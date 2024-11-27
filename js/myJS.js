cd32f2b23e2f40b758ed763fb8f3933f64855471
khalilTaj <khalil.taj.aldean@gmail.com>
2024-11-27 03:08:49 +0200

    js varubales explained

--- js/myJS.js
+++ js/myJS.js
@@ -1,1 +1,24 @@
-console.log("jefff")
+console.log("hello world");
+
+let name = "jeff";
+let lastName = "herman";
+
+console.log(name, lastName);
+console.log(lastName);
+console.log(name);
+
+// re-assigning 
+
+let age = 11;
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14159;
+console.log(pi);
+
+pi = 50;
+console.log(pi);
+
