408f3e5de71efe80633f8a9ddb42fd7313e4447d
khalilTaj <khalil.taj.aldean@gmail.com>
2025-08-14 23:54:04 +0300

    for loop syntex

---
+++ js/loops and arrays.js
@@ -0,0 +1,9 @@
+// for loop explained on an array
+
+const cats = ["Leopard", "Serval", "Jaguar", "Tiger", "Caracal", "Lion"];
+
+for (const cat of cats) {
+    //* here we created the const `cat` and assigend it for every value of the array `cats`
+    console.log(cat);
+}
+
