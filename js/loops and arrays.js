0758bb59531ce64f9a8ddf14021130e711add338
khalilTaj <khalil.taj.aldean@gmail.com>
2025-08-15 19:24:45 +0300

    loop through a collection with map() and filter()

--- js/loops and arrays.js
+++ js/loops and arrays.js
@@ -7,3 +7,20 @@ for (const cat of cats) {
     console.log(cat);
 }
 
+// spicailty loops map() and filter()\
+//* map() apply the needed changes and creat a new collection for it
+
+function toUpper(string) {
+  return string.toUpperCase();
+}
+
+const upperCats = cats.map(toUpper);
+console.log(upperCats);
+
+//* filter() is used to creat a new collection with the items that match
+function lCat(cat) {
+  return cat.startsWith("L");
+};
+const filtered = cats.filter(lCat);
+
+console.log(filtered);
